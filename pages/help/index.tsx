4659459aabc072f3116c936b8daf9729a863f19f
mhmAdmin1122 <hasaanad12min@gmail.com>
2023-09-18 13:01:40 +0500

    help page created

---
+++ pages/help/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../Components/L/Layout'
+
+const index = () => {
+  return (
+    <Layout>
+      Help page
+    </Layout>
+  )
+}
+
+export default index
